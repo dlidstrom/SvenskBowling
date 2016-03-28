85025daad916243020d3321f8923b7c7066f2db5
Daniel Lidstrom <dlidstrom@gmail.com>
2016-03-28 12:57:41 +0200

    Prepared for redux

---
+++ actions/actions.js
@@ -0,0 +1,20 @@
+import ActionTypes from './actionTypes.js'
+
+const addTodo = (text) => {
+  return {
+    type: ActionTypes.ADD_TODO,
+    text
+  };
+};
+
+const removeTodo = (id) => {
+  return {
+    type: ActionTypes.REMOVE_TODO,
+    id
+  }
+}
+
+export default {
+  addTodo,
+  removeTodo
+};
